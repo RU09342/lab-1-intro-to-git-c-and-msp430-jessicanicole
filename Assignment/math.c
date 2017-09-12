48033f60bd3e2521a0b4e063c6e85388b11b498c
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-09-12 15:34:21 -0400

    Code written for math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include "Math.h"
 
+//Function declaration
 int math(int num1, int num2, char op)
 {
 	int result;
 	switch (op)
 	{
+	//Addition
 	case '+':
 	{
 		result = num1 + num2;
 		return result;
 	}
+	//Subtraction 
 	case '-':
 	{
 		result = num1 - num2;
 		return result;
 	}
+	//Multiplication
 	case '*':
 	{
 		result = num1 * num2;
 		return result;
 	}
+	//Division
 	case '/':
 	{
 		result = num1 / num2;
 		return result;
 	}
+	//Modulus 
 	case '%':
 	{
 		result = num1 % num2;
